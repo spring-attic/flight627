fbbaccc76a5ba0b4794b24f9465d8a8ed0a7d311
Martin Lippert <mlippert@gopivotal.com>
2013-09-08 00:20:25 +0200

    added missing copyright statements

--- eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/CloudSyncResourceListener.java
+++ eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/CloudSyncResourceListener.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *  Copyright (c) 2013 GoPivotal, Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *      GoPivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.springsource.ide.eclipse.cloudsync.internal;
 
 import org.eclipse.core.resources.IProject;
@@ -7,6 +17,9 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
 
+/**
+ * @author Martin Lippert
+ */
 public class CloudSyncResourceListener implements IResourceChangeListener {
 
 	private CloudSyncController controller;
--- eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/CloudSyncService.java
+++ eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/CloudSyncService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *  Copyright (c) 2013 GoPivotal, Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *      GoPivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.springsource.ide.eclipse.cloudsync.internal;
 
 import java.io.ByteArrayInputStream;
@@ -23,6 +33,9 @@ import org.eclipse.core.runtime.CoreException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+/**
+ * @author Martin Lippert
+ */
 public class CloudSyncService {
 	
 	private String api;
@@ -369,7 +382,6 @@ public class CloudSyncService {
 			flag = true;
 		}
 		result.append("]");
-		System.err.println(" =========> " + result);
 		return result.toString();
 	}
 
