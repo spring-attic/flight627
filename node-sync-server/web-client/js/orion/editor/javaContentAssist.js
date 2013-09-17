e095a4110088f855375903b09502ff6e20f86cca
Martin Lippert <mlippert@gopivotal.com>
2013-09-17 22:33:51 +0200

    added missing license header

--- node-sync-server/web-client/js/orion/editor/javaContentAssist.js
+++ node-sync-server/web-client/js/orion/editor/javaContentAssist.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * @license
+ *  Copyright (c) 2013 GoPivotal, Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *      GoPivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 define("orion/editor/javaContentAssist", ['orion/editor/Deferred'], function(Deferred) {
 	
 	var currentCallbackId = 0;
