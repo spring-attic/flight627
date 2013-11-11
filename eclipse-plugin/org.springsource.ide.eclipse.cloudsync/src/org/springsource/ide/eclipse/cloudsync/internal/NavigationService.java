b5d6994c65501415f8a5c178f47037dc933f1ada
Martin Lippert <mlippert@gopivotal.com>
2013-11-11 21:50:10 +0100

    removed unused code

--- eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/NavigationService.java
+++ eclipse-plugin/org.springsource.ide.eclipse.cloudsync/src/org/springsource/ide/eclipse/cloudsync/internal/NavigationService.java
@@ -11,14 +11,11 @@
 package org.springsource.ide.eclipse.cloudsync.internal;
 
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jdt.core.CompletionProposal;
-import org.eclipse.jdt.core.IAccessRule;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.ISourceReference;
 import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.Signature;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -65,90 +62,6 @@ public class NavigationService {
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	protected String getDescription(CompletionProposal proposal) {
-		StringBuilder description = new StringBuilder();
-		description.append("{");
-
-		if (proposal.getKind() == CompletionProposal.METHOD_REF) {
-			description.append("\"icon\":{\"src\":\"../js/editor/textview/methpub_obj.gif\"},");
-			description.append("\"segments\": ");
-			description.append("[");
-
-			String sig = Signature.toString(new String(proposal.getSignature()), new String(proposal.getName()), null, false, false);
-
-			description.append("{");
-			String result = sig + " : " + Signature.getSimpleName(Signature.toString(Signature.getReturnType(new String(proposal.getSignature()))));
-			description.append("\"value\":\"" + result + "\"");
-			description.append("}");
-
-			description.append(",");
-			description.append("{");
-			String appendix = " - " + Signature.getSignatureSimpleName(new String(proposal.getDeclarationSignature()));
-			description.append("\"value\":\"" + appendix + "\",");
-			description.append("\"style\":{");
-			description.append("\"color\":\"#AAAAAA\"");
-			description.append("}");
-			description.append("}");
-
-			description.append("]");
-
-		} else if (proposal.getKind() == CompletionProposal.FIELD_REF) {
-			description.append("\"icon\":{\"src\":\"../js/editor/textview/field_public_obj.gif\"},");
-			description.append("\"segments\": ");
-			description.append("[");
-
-			description.append("{");
-			String result = new String(proposal.getCompletion()) + " : "
-					+ (proposal.getSignature() != null ? Signature.getSignatureSimpleName(new String(proposal.getSignature())) : "<unknown>");
-			description.append("\"value\":\"" + result + "\"");
-			description.append("}");
-
-			description.append(",");
-			description.append("{");
-			String appendix = " - "
-					+ (proposal.getDeclarationSignature() != null ? Signature.getSignatureSimpleName(new String(proposal.getDeclarationSignature()))
-							: "<unknown>");
-			description.append("\"value\":\"" + appendix + "\",");
-			description.append("\"style\":{");
-			description.append("\"color\":\"#AAAAAA\"");
-			description.append("}");
-			description.append("}");
-
-			description.append("]");
-
-		} else if (proposal.getKind() == CompletionProposal.TYPE_REF) {
-			if (proposal.getAccessibility() == IAccessRule.K_NON_ACCESSIBLE) {
-				return null;
-			}
-
-			description.append("\"icon\":{\"src\":\"../js/editor/textview/class_obj.gif\"},");
-			description.append("\"segments\": ");
-			description.append("[");
-
-			description.append("{");
-			String result = Signature.getSignatureSimpleName(new String(proposal.getSignature()));
-			description.append("\"value\":\"" + result + "\"");
-			description.append("}");
-
-			description.append(",");
-			description.append("{");
-			String appendix = " - " + new String(proposal.getDeclarationSignature());
-			description.append("\"value\":\"" + appendix + "\",");
-			description.append("\"style\":{");
-			description.append("\"color\":\"#AAAAAA\"");
-			description.append("}");
-			description.append("}");
-
-			description.append("]");
-
-		} else {
-			return null;
-		}
-
-		description.append("}");
-		return description.toString();
 	}
 
 }
