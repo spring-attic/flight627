3b515c7968e0961876bdbe50e740f5e96b1d67fb
Martin Lippert <mlippert@gopivotal.com>
2014-01-13 19:41:38 -0800

    improved live edit unit handling for open editors in Eclipse
    Conflicts:
    	eclipse.core/org.eclipse.flight.core/src/org/eclipse/flight/core/Repository.java
    	eclipse.ui/org.eclipse.flight.ui.integration/src/org/eclipse/flight/ui/integration/handlers/LiveEditConnector.java

--- eclipse.ui/org.eclipse.flight.ui.integration/src/org/eclipse/flight/ui/integration/handlers/LiveEditConnector.java
+++ eclipse.ui/org.eclipse.flight.ui.integration/src/org/eclipse/flight/ui/integration/handlers/LiveEditConnector.java
@@ -7,17 +7,25 @@
  *
  * Contributors:
  *     Pivotal Software, Inc. - initial API and implementation
-*******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.flight.ui.integration.handlers;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.filebuffers.IFileBuffer;
+import org.eclipse.core.filebuffers.IFileBufferListener;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.flight.core.ConnectedProject;
 import org.eclipse.flight.core.ConnectedResource;
 import org.eclipse.flight.core.ILiveEditConnector;
+import org.eclipse.flight.core.IRepositoryListener;
 import org.eclipse.flight.core.LiveEditCoordinator;
 import org.eclipse.flight.core.WorkspaceRepository;
 import org.eclipse.flight.objects.Resource;
@@ -26,6 +34,8 @@ import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchPartReference;
@@ -37,35 +47,125 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
  * @author Martin Lippert
  */
 public class LiveEditConnector {
-	
+
 	private static final String LIVE_EDIT_CONNECTOR_ID = "UI-Editor-Live-Edit-Connector";
-	
+
 	private IDocumentListener documentListener;
+
 	private WorkspaceRepository repository;
-	
+
 	private ConcurrentMap<IDocument, String> resourceMappings;
+
 	private ConcurrentMap<String, IDocument> documentMappings;
+
 	private LiveEditCoordinator liveEditCoordinator;
 
 	public LiveEditConnector(LiveEditCoordinator liveEditCoordinator, WorkspaceRepository repository) {
 		this.liveEditCoordinator = liveEditCoordinator;
 		this.repository = repository;
-		
+
 		this.resourceMappings = new ConcurrentHashMap<IDocument, String>();
 		this.documentMappings = new ConcurrentHashMap<String, IDocument>();
-		
+
 		this.documentListener = new IDocumentListener() {
 			@Override
 			public void documentChanged(DocumentEvent event) {
 				sendModelChangedMessage(event);
 			}
+
 			@Override
 			public void documentAboutToBeChanged(DocumentEvent event) {
 			}
 		};
-		
+
+		FileBuffers.getTextFileBufferManager().addFileBufferListener(new IFileBufferListener() {
+			@Override
+			public void underlyingFileMoved(IFileBuffer buffer, IPath path) {
+			}
+
+			@Override
+			public void underlyingFileDeleted(IFileBuffer buffer) {
+			}
+
+			@Override
+			public void stateValidationChanged(IFileBuffer buffer, boolean isStateValidated) {
+			}
+
+			@Override
+			public void stateChanging(IFileBuffer buffer) {
+			}
+
+			@Override
+			public void stateChangeFailed(IFileBuffer buffer) {
+			}
+
+			@Override
+			public void dirtyStateChanged(IFileBuffer buffer, boolean isDirty) {
+			}
+
+			@Override
+			public void bufferDisposed(IFileBuffer buffer) {
+			}
+
+			@Override
+			public void bufferCreated(IFileBuffer buffer) {
+			}
+
+			@Override
+			public void bufferContentReplaced(IFileBuffer buffer) {
+				IPath path = buffer.getLocation();
+
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				IWorkspaceRoot root = workspace.getRoot();
+				IResource resource = root.findMember(path);
+				if (resource != null) {
+					IProject project = resource.getProject();
+					String resourcePath = resource.getProjectRelativePath().toString();
+
+					String fullPath = project.getName() + "/" + resourcePath;
+					IDocument doc = documentMappings.get(fullPath);
+					if (doc != null) {
+						doc.addDocumentListener(documentListener);
+					}
+				}
+
+				System.out.println("content replaced by new version on the file system");
+			}
+
+			@Override
+			public void bufferContentAboutToBeReplaced(IFileBuffer buffer) {
+				IPath path = buffer.getLocation();
+
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				IWorkspaceRoot root = workspace.getRoot();
+				IResource resource = root.findMember(path);
+				if (resource != null) {
+					IProject project = resource.getProject();
+					String resourcePath = resource.getProjectRelativePath().toString();
+
+					String fullPath = project.getName() + "/" + resourcePath;
+					IDocument doc = documentMappings.get(fullPath);
+					if (doc != null) {
+						doc.removeDocumentListener(documentListener);
+					}
+				}
+			}
+		});
+
+		this.repository.addRepositoryListener(new IRepositoryListener() {
+			@Override
+			public void projectConnected(IProject project) {
+				connectOpenEditors(project);
+			}
+
+			@Override
+			public void projectDisconnected(IProject project) {
+				disconnectOpenEditors(project);
+			}
+		});
+
 		ILiveEditConnector liveEditConnector = new ILiveEditConnector() {
-			
+
 			@Override
 			public String getConnectorID() {
 				return LIVE_EDIT_CONNECTOR_ID;
@@ -87,7 +187,7 @@ public class LiveEditConnector {
 			}
 		};
 		this.liveEditCoordinator.addLiveEditConnector(liveEditConnector);
-		
+
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
 				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
@@ -95,6 +195,7 @@ public class LiveEditConnector {
 					@Override
 					public void partVisible(IWorkbenchPartReference partRef) {
 					}
+
 					@Override
 					public void partOpened(IWorkbenchPartReference partRef) {
 						IWorkbenchPart part = partRef.getPart(false);
@@ -102,15 +203,19 @@ public class LiveEditConnector {
 							connectEditor((AbstractTextEditor) part);
 						}
 					}
+
 					@Override
 					public void partInputChanged(IWorkbenchPartReference partRef) {
 					}
+
 					@Override
 					public void partHidden(IWorkbenchPartReference partRef) {
 					}
+
 					@Override
 					public void partDeactivated(IWorkbenchPartReference partRef) {
 					}
+
 					@Override
 					public void partClosed(IWorkbenchPartReference partRef) {
 						IWorkbenchPart part = partRef.getPart(false);
@@ -118,9 +223,11 @@ public class LiveEditConnector {
 							disconnectEditor((AbstractTextEditor) part);
 						}
 					}
+
 					@Override
 					public void partBroughtToTop(IWorkbenchPartReference partRef) {
 					}
+
 					@Override
 					public void partActivated(IWorkbenchPartReference partRef) {
 					}
@@ -128,6 +235,7 @@ public class LiveEditConnector {
 			}
 		});
 	}
+
 	protected void remoteEditorStarted(Edit edit) {
 		// a different editor was started editing the resource, we need to send back live content
 		if (this.repository.getUsername().equals(edit.getUserName()) && documentMappings.containsKey(edit.getFullPath())) {
@@ -144,24 +252,24 @@ public class LiveEditConnector {
 
 			ConnectedProject connectedProject = (ConnectedProject) repository.getProject(edit.getProjectName());
 			final ConnectedResource localResource = (ConnectedResource) connectedProject.getResource(edit.getPath());
-			
+
 			if (localResource.getHash() != null && localResource.getHash().equals(edit.getSavePointHash()) && localResource.getTimestamp() == edit.getSavePointTimestamp()) {
 				String openedContent = document.get();
 				if (!openedContent.equals(localResource.getData())) {
 					try {
-						Display.getDefault().asyncExec(new Runnable() {
-							public void run() {
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
 								try {
-									document.removeDocumentListener(documentListener);
-									document.set(localResource.getData());
-									document.addDocumentListener(documentListener);
-								}
+							document.removeDocumentListener(documentListener);
+							document.set(localResource.getData());
+							document.addDocumentListener(documentListener);
+						}
 								catch (Exception e) {
 									e.printStackTrace();
 								}
 							}
-						});
-					}
+					});
+				}
 					catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -173,7 +281,7 @@ public class LiveEditConnector {
 	protected void handleModelChanged(final Edit edit) {
 		if (repository.getUsername().equals(edit.getUserName()) && edit.getFullPath() != null && documentMappings.containsKey(edit.getFullPath())) {
 			final IDocument document = documentMappings.get(edit.getFullPath());
-			
+
 			try {
 				Display.getDefault().asyncExec(new Runnable() {
 					public void run() {
@@ -214,32 +322,62 @@ public class LiveEditConnector {
 	protected void connectEditor(AbstractTextEditor texteditor) {
 		final IDocument document = texteditor.getDocumentProvider().getDocument(texteditor.getEditorInput());
 		IResource eclipseResource = (IResource) texteditor.getEditorInput().getAdapter(IResource.class);
-		
+
 		if (document != null && eclipseResource != null) {
 			IProject project = eclipseResource.getProject();
 			String resourcePath = eclipseResource.getProject().getName() + "/" + eclipseResource.getProjectRelativePath().toString();
-			
+
 			if (repository.isConnected(project)) {
 				documentMappings.put(resourcePath, document);
 				resourceMappings.put(document, resourcePath);
 
 				document.addDocumentListener(documentListener);
-				
+
 				ConnectedProject connectedProject = (ConnectedProject) repository.getProject(project);
 				Resource resource = connectedProject.getResource(eclipseResource.getProjectRelativePath().toString());
 				this.liveEditCoordinator.sendLiveEditStartedMessage(resource.toEdit());
 			}
 		}
 	}
-	
+
 	protected void disconnectEditor(AbstractTextEditor texteditor) {
 		final IDocument document = texteditor.getDocumentProvider().getDocument(texteditor.getEditorInput());
-		
+
 		String resourcePath = resourceMappings.get(document);
-		
+
 		document.removeDocumentListener(documentListener);
 		documentMappings.remove(resourcePath);
 		resourceMappings.remove(document);
+	}
+
+	protected void connectOpenEditors(IProject project) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				IEditorReference[] editorReferences = window.getActivePage().getEditorReferences();
+				for (IEditorReference editorReference : editorReferences) {
+					IEditorPart editorPart = editorReference.getEditor(false);
+					if (editorPart instanceof AbstractTextEditor) {
+						connectEditor((AbstractTextEditor) editorPart);
+					}
+				}
+			}
+		});
+	}
+
+	protected void disconnectOpenEditors(IProject project) {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				IEditorReference[] editorReferences = window.getActivePage().getEditorReferences();
+				for (IEditorReference editorReference : editorReferences) {
+					IEditorPart editorPart = editorReference.getEditor(false);
+					if (editorPart instanceof AbstractTextEditor) {
+						disconnectEditor((AbstractTextEditor) editorPart);
+					}
+				}
+			}
+		});
 	}
 
 }
